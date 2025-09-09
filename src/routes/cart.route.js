91ab97d43abec2a36a6d7e98a36defe81960e4c1
sahilsOfficialCode <sahilsajith.official@gmail.com>
2025-09-09 18:41:48 +0530

    route for cart

---
+++ src/routes/cart.route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const cartController = require("../controller/cart.controller");
+
+router.post("/add", cartController.addToCart);
+router.get("/", cartController.getCart);
+router.put("/update", cartController.updateCartItem);
+router.delete("/remove/:productId", cartController.removeFromCart);
+
+module.exports = router;
