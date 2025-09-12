a82e1a8bbc2f21f8e2e46bf6321837db49ad8176
vipin-astria <vipin.m@astriadigital.com>
2025-09-13 00:48:28 +0530

    index file updated

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -4,7 +4,7 @@ const ErrorHandler = require("../utils/errorHandler");
 exports.getIndex = catchAsyncErrors(async(req, res, next)=>{
      res.render("index", { 
     title: "Home Page", 
-    message: "Welcome to Ecommerce Project!", 
+    message: "Welcome to Ecommerce Projects!", 
     tagline: "Fast · Secure · Reliable"
   });
 })
