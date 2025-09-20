2faba732d00f587174e1765d684f7889f26f639b
sahilsOfficialCode <sahilsajith.official@gmail.com>
2025-09-20 18:11:05 +0530

    razorpay package added

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../controller/order.controller");
+const { userAuthentication } = require("../middleware/auth");
 
+router.use(userAuthentication);
 router.post("/create", orderController.createOrder);
 router.post("/verify-payment", orderController.verifyPayment);
 router.get("/", orderController.getUserOrders);
